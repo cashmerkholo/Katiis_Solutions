e26ff22d561d8e290da851bd6ba73917504db13c
cashmerkholo <171548020+cashmerkholo@users.noreply.github.com>
2024-07-30 19:33:33 +0100

    Create R2.py

---
+++ Easy/R2.py
@@ -0,0 +1,21 @@
+<math xmlns="http://www.w3.org/1998/Math/MathML">
+  <mo stretchy="false">(</mo>
+  <msub>
+    <mi>R</mi>
+    <mn>1</mn>
+  </msub>
+  <mo>+</mo>
+  <msub>
+    <mi>R</mi>
+    <mn>2</mn>
+  </msub>
+  <mo stretchy="false">)</mo>
+  <mrow data-mjx-texclass="ORD">
+    <mo>/</mo>
+  </mrow>
+  <mn>2</mn>
+</math>
+
+n = input()
+a,b=n.split(" ")
+print(2*int(b)-int(a))
